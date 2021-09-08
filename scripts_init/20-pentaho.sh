2c1c9e17960c82d1e79c074181b1ab3a016c2a32
Adriano Sales Santos <sales.adriano@gmail.com>
2021-09-07 23:31:05 -0500

    Ajuste imagem

--- scripts_init/20-pentaho.sh
+++ scripts_init/20-pentaho.sh
@@ -2,7 +2,7 @@
 wget -O pentaho.zip https://sourceforge.net/projects/pentaho/files/latest/download
 unzip pentaho.zip
 mkdir /pentaho
-mv data-integration/* pentaho/
+mv data-integration/* /pentaho/
 rm -Rf data-integration pentaho.zip
 rsync /code/drivers/* /pentaho/lib/
     
